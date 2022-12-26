426b36d43dee011dcbf9676775bab5203725a0b5
William Kamau <william.kamuyu@gapstack.com>
2022-12-26 13:49:25 +0300

    Removed outline on tabs

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,42 +1,38 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import { Tab } from '@headlessui/react'
 
-export const Tabs = () => {
+interface item {
+    label: string;
+    key: string;
+    content: ReactNode
+    disabled?: boolean;
+}
+
+export interface TabsProps {
+    defaultActiveKey: string;
+    items: item[];
+}
+
+export const Tabs = ({ items }: TabsProps) => {
     return (
         <Tab.Group>
             <Tab.List>
-                <Tab>
-                    {({ selected }) => (
-                        <button
-                            className={`${selected ? 'text-orange-500 border-b-orange-500' : 'text-gray-500'} px-4 pb-2.5 font-medium border-b`}
-                        >
-                            Tab 1
-                        </button>
-                    )}
-                </Tab>
-                <Tab>
-                    {({ selected }) => (
-                        <button
-                            className={`${selected ? 'text-orange-500 border-b-orange-500' : 'text-gray-500'} px-4 pb-2.5 font-medium border-b`}
-                        >
-                            Tab 2
-                        </button>
-                    )}
-                </Tab>
-                <Tab>
-                    {({ selected }) => (
-                        <button
-                            className={`${selected ? 'text-orange-500 border-b-orange-500' : 'text-gray-500'} px-4 pb-2.5 font-medium border-b`}
-                        >
-                            Tab 3
-                        </button>
-                    )}
-                </Tab>
+                {items.map(item => (
+                    <Tab className="outline-0">
+                        {({ selected }) => (
+                            <button
+                                className={`${selected ? 'text-orange-500 border-b-orange-500' : 'text-gray-500'} px-4 pb-2.5 font-medium border-b`}
+                            >
+                                {item.label}
+                            </button>
+                        )}
+                    </Tab>
+                ))}
             </Tab.List>
             <Tab.Panels>
-                <Tab.Panel>Content 1</Tab.Panel>
-                <Tab.Panel>Content 2</Tab.Panel>
-                <Tab.Panel>Content 3</Tab.Panel>
+                { items.map(item => (
+                    <Tab.Panel>{item.content}</Tab.Panel>
+                ))}
             </Tab.Panels>
         </Tab.Group>
     );
--- stories/Tabs.stories.tsx
+++ stories/Tabs.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Tabs } from "../src";
+import {Tabs, TabsProps} from "../src";
 
 const meta: Meta = {
     title: 'Data Display/Tabs',
@@ -9,7 +9,14 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => <Tabs {...args} />;
+const Template: Story<TabsProps> = (args) => <Tabs {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+    defaultActiveKey: '1',
+    items: [
+        { label: 'Tab 1', key: '1', content: <p>Content 1</p>},
+        { label: 'Tab 2', key: '2', content: <p>Content 2</p>},
+        { label: 'Tab 3', key: '3', content: <p>Content 3</p>},
+    ]
+};
