d7cd6ae2f582686f9767ce46a8968c338413d3d9
William Kamau <william.kamuyu@gapstack.com>
2023-01-03 11:38:34 +0300

    Updated product tile

--- src/ProductTile.tsx
+++ src/ProductTile.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button, ButtonTypes } from "./Button";
-import { RiHeart3Line } from "react-icons/all";
+import { RiHeart3Line, RiHeart3Fill } from "react-icons/all";
+import colors from "tailwindcss/colors";
 
 export interface ProductTileProps {
   imageUrl: string;
   name?: string;
   shop?: string;
   price?: string;
+  addedToWishList?: boolean
 }
 
-export const ProductTile = ({ imageUrl, name, shop, price }: ProductTileProps) => {
+export const ProductTile = ({ imageUrl, name, shop, price, addedToWishList }: ProductTileProps) => {
+  const [inWishList] = useState<boolean>(addedToWishList || false);
+
   return(
-      <div className="w-full h-full rounded cursor-pointer">
+      <div className="group/product-tile w-full h-full rounded cursor-pointer">
         <div className="w-full h-full relative rounded bg-gray-100 flex justify-center items-center">
             <img src={imageUrl} alt={name} />
-            <RiHeart3Line className="absolute z-20 top-3 right-3" />
+            {
+                inWishList ? (
+                    <RiHeart3Fill color={colors.red["500"]} className="absolute z-20 top-3 right-3" />
+                ) : <RiHeart3Line className="absolute z-20 top-3 right-3 invisible group-hover/product-tile:visible" />
+
+            }
         </div>
         <p className="text-xs leading-4 font-semibold mt-2.5 text-gray-800">{name}</p>
         <p className="text-xs leading-4 font-normal mt-1.5 text-gray-500">Dutch Green | <Button onClick={() => console.log('clicked')} className="py-0" type={ButtonTypes.link}>{shop}</Button></p>
--- stories/ProductTile.stories.tsx
+++ stories/ProductTile.stories.tsx
@@ -18,5 +18,6 @@ Default.args = {
     imageUrl: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png',
     name: 'Air Jordan 1 Retro High OG',
     shop: 'Nike',
-    price: 'KSH 12,200'
+    price: 'KSH 12,200',
+    addedToWishList: true
 };
