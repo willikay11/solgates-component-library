d511e543e72df90a34cdf864d1a3df4a6c13bc53
William Kamau <william.kamuyu@gapstack.com>
2023-01-03 12:27:13 +0300

    Added view for individual product

--- src/ProductTile.tsx
+++ src/ProductTile.tsx
@@ -4,20 +4,21 @@ import {RiHeart3Fill, RiHeart3Line, RiArrowRightLine } from "react-icons/all";
 import colors from "tailwindcss/colors";
 
 export enum PRODUCT_TYPE {
+   PRODUCTS = 'products',
    PRODUCT = 'product',
    COLLECTION = 'collection'
 }
 
 export interface ProductTileProps {
   imageUrl: string;
-  type?: PRODUCT_TYPE.PRODUCT | PRODUCT_TYPE.COLLECTION;
+  type?: PRODUCT_TYPE.PRODUCTS | PRODUCT_TYPE.PRODUCT | PRODUCT_TYPE.COLLECTION;
   name?: string;
   shop?: string;
   price?: string;
   addedToWishList?: boolean
 }
 
-export const ProductTile = ({ imageUrl, type = PRODUCT_TYPE.PRODUCT, name, shop, price, addedToWishList }: ProductTileProps) => {
+export const ProductTile = ({ imageUrl, type = PRODUCT_TYPE.PRODUCTS, name, shop, price, addedToWishList }: ProductTileProps) => {
   const [inWishList] = useState<boolean>(addedToWishList || false);
 
   return(
@@ -25,19 +26,19 @@ export const ProductTile = ({ imageUrl, type = PRODUCT_TYPE.PRODUCT, name, shop,
         <div className="w-full h-full relative rounded bg-gray-100 flex justify-center items-center">
             <img src={imageUrl} alt={name} />
             {
-                type === PRODUCT_TYPE.PRODUCT? (inWishList ?
+                type === PRODUCT_TYPE.PRODUCTS? (inWishList ?
                         <RiHeart3Fill color={colors.red["500"]} className="absolute z-20 top-3 right-3" />
                  : <RiHeart3Line className="absolute z-20 top-3 right-3 invisible group-hover/product-tile:visible" />
-                ) : (
+                ) : type === PRODUCT_TYPE.COLLECTION ?(
                     <div className="absolute z-20 top-3 left-3">
                         <p className="text-base leading-6 font-bold text-gray-800">{name}</p>
                         <Button onClick={() => console.log('clicked')} type={ButtonTypes.link} className="bg-transparent py-0">Shop Collection <RiArrowRightLine /></Button>
                     </div>
-                )
+                ) : null
             }
         </div>
         {
-            type === PRODUCT_TYPE.PRODUCT && (
+            type === PRODUCT_TYPE.PRODUCTS && (
                 <>
                     <p className="text-xs leading-4 font-semibold mt-2.5 text-gray-800">{name}</p>
                     <p className="text-xs leading-4 font-normal mt-1.5 text-gray-500">Dutch Green | <Button onClick={() => console.log('clicked')} className="py-0" type={ButtonTypes.link}>{shop}</Button></p>
--- stories/ProductTile.stories.tsx
+++ stories/ProductTile.stories.tsx
@@ -13,12 +13,18 @@ const Template: Story<ProductTileProps> = (args) => <div className="grid gap-4 g
     <ProductTile {...args} />
 </div>;
 
-export const Product = Template.bind({});
-Product.args = {
+export const Products = Template.bind({});
+Products.args = {
     imageUrl: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png',
     name: 'Air Jordan 1 Retro High OG',
     shop: 'Nike',
     price: 'KSH 12,200',
+};
+
+export const Product = Template.bind({});
+Product.args = {
+    imageUrl: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png',
+    type: PRODUCT_TYPE.PRODUCT,
 };
 
 export const Collection = Template.bind({});
