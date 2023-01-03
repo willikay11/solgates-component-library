61a5b3d10317ec2b1c31adaa70f6b15e7534bc9a
William Kamau <william.kamuyu@gapstack.com>
2023-01-03 11:56:02 +0300

    Added collection to product tile

--- src/ProductTile.tsx
+++ src/ProductTile.tsx
@@ -1,17 +1,23 @@
 import React, {useState} from "react";
-import { Button, ButtonTypes } from "./Button";
-import { RiHeart3Line, RiHeart3Fill } from "react-icons/all";
+import {Button, ButtonTypes} from "./Button";
+import {RiHeart3Fill, RiHeart3Line, RiArrowRightLine } from "react-icons/all";
 import colors from "tailwindcss/colors";
+
+export enum PRODUCT_TYPE {
+   PRODUCT = 'product',
+   COLLECTION = 'collection'
+}
 
 export interface ProductTileProps {
   imageUrl: string;
+  type?: PRODUCT_TYPE.PRODUCT | PRODUCT_TYPE.COLLECTION;
   name?: string;
   shop?: string;
   price?: string;
   addedToWishList?: boolean
 }
 
-export const ProductTile = ({ imageUrl, name, shop, price, addedToWishList }: ProductTileProps) => {
+export const ProductTile = ({ imageUrl, type = PRODUCT_TYPE.PRODUCT, name, shop, price, addedToWishList }: ProductTileProps) => {
   const [inWishList] = useState<boolean>(addedToWishList || false);
 
   return(
@@ -19,15 +25,26 @@ export const ProductTile = ({ imageUrl, name, shop, price, addedToWishList }: Pr
         <div className="w-full h-full relative rounded bg-gray-100 flex justify-center items-center">
             <img src={imageUrl} alt={name} />
             {
-                inWishList ? (
-                    <RiHeart3Fill color={colors.red["500"]} className="absolute z-20 top-3 right-3" />
-                ) : <RiHeart3Line className="absolute z-20 top-3 right-3 invisible group-hover/product-tile:visible" />
-
+                type === PRODUCT_TYPE.PRODUCT? (inWishList ?
+                        <RiHeart3Fill color={colors.red["500"]} className="absolute z-20 top-3 right-3" />
+                 : <RiHeart3Line className="absolute z-20 top-3 right-3 invisible group-hover/product-tile:visible" />
+                ) : (
+                    <div className="absolute z-20 top-3 left-3">
+                        <p className="text-base leading-6 font-bold text-gray-800">{name}</p>
+                        <Button onClick={() => console.log('clicked')} type={ButtonTypes.link} className="bg-transparent py-0">Shop Collection <RiArrowRightLine /></Button>
+                    </div>
+                )
             }
         </div>
-        <p className="text-xs leading-4 font-semibold mt-2.5 text-gray-800">{name}</p>
-        <p className="text-xs leading-4 font-normal mt-1.5 text-gray-500">Dutch Green | <Button onClick={() => console.log('clicked')} className="py-0" type={ButtonTypes.link}>{shop}</Button></p>
-        <p className="text-xs leading-4 font-semibold mt-1.5 text-gray-800">{price}</p>
+        {
+            type === PRODUCT_TYPE.PRODUCT && (
+                <>
+                    <p className="text-xs leading-4 font-semibold mt-2.5 text-gray-800">{name}</p>
+                    <p className="text-xs leading-4 font-normal mt-1.5 text-gray-500">Dutch Green | <Button onClick={() => console.log('clicked')} className="py-0" type={ButtonTypes.link}>{shop}</Button></p>
+                    <p className="text-xs leading-4 font-semibold mt-1.5 text-gray-800">{price}</p>
+                </>
+            )
+        }
       </div>
   );
 }
--- stories/ProductTile.stories.tsx
+++ stories/ProductTile.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { ProductTile } from "../src";
+import {PRODUCT_TYPE, ProductTile, ProductTileProps} from "../src";
 
 const meta: Meta = {
     title: 'Data Display/Product Tile',
@@ -9,15 +9,21 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => <div className="grid gap-4 grid-cols-4">
-    <ProductTile imageUrl="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png" name="Air Jordan 1 Retro High OG" price="KSH 20,000" shop="Nike" {...args} />
+const Template: Story<ProductTileProps> = (args) => <div className="grid gap-4 grid-cols-4">
+    <ProductTile {...args} />
 </div>;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Product = Template.bind({});
+Product.args = {
     imageUrl: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png',
     name: 'Air Jordan 1 Retro High OG',
     shop: 'Nike',
     price: 'KSH 12,200',
-    addedToWishList: true
+};
+
+export const Collection = Template.bind({});
+Collection.args = {
+    type: PRODUCT_TYPE.COLLECTION,
+    imageUrl: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/23696ebc-18fc-46f9-8a88-c854930ad24c/air-jordan-1-retro-high-womens-shoes-W1tGx4.png',
+    name: 'Jordans'
 };
