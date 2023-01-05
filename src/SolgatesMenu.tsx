05fb152281ac68ab14a1462f1460eede14e2415c
William Kamau <william.kamuyu@gapstack.com>
2023-01-05 09:58:41 +0300

    Updated menu

--- src/SolgatesMenu.tsx
+++ src/SolgatesMenu.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Menu, Transition } from '@headlessui/react'
-import { RiUser6Line, RiHeart3Line, RiShoppingBagLine, RiSearch2Line } from "react-icons/ri";
+import {RiUser6Line, RiHeart3Line, RiShoppingBagLine, RiSearch2Line, RiMenu4Fill} from "react-icons/ri";
 import { Input } from "./Input";
 import colors from "tailwindcss/colors";
 
@@ -29,8 +29,14 @@ export interface SolgatesMenuProps {
 
 export const SolgatesMenu = ({ menus, logoUrl }: SolgatesMenuProps) => {
     return (
-        <div className="fixed relative w-full inline-flex h-[80px] items-center border-b border-gray-100">
-            <div className="grid grid-cols-12 w-full h-full">
+        <div className="fixed relative w-full h-[60px] md:h-[120px] lg:h-[120px] items-center border-b border-gray-100">
+            <div className="bg-gray-100 flex py-[2px] lg:grid lg:grid-cols-12 w-full">
+                <div className="md:col-start-2 md:col-span-8 lg:col-start-3 lg:col-span-8 flex w-full items-center justify-center md:justify-end lg:justify-end">
+                    <span className="text-[10px] text-gray-600 mr-2">Help</span>
+                    <span className="text-[10px] text-gray-600 mr-2">Open Shop</span>
+                </div>
+            </div>
+            <div className="hidden md:grid lg:grid grid-cols-12 w-full" style={{ height: 'calc(120px - 20px)'}}>
                 <div className="md:col-start-2 md:col-span-8 lg:col-start-3 lg:col-span-6 inline-flex">
                     <img src={logoUrl} className="w-[90px] mr-[30px]" />
                     {
@@ -50,7 +56,7 @@ export const SolgatesMenu = ({ menus, logoUrl }: SolgatesMenuProps) => {
                                     leaveFrom="transform opacity-100 scale-100"
                                     leaveTo="transform opacity-0 scale-95"
                                 >
-                                    <Menu.Items className="absolute right-0 top-[72px] mt-2 w-full origin-top-right divide-y divide-gray-100 bg-white shadow-xl focus:outline-none">
+                                    <Menu.Items className="absolute right-0 top-[114px] mt-2 w-full origin-top-right divide-y divide-gray-100 bg-white shadow-xl focus:outline-none">
                                         <div className={`grid grid-cols-12 my-[30px]`}>
                                             <div className="md:col-start-2 md:col-span-9 lg:col-start-3 lg:col-span-8 inline-flex">
                                                 <div className={`grid gap-${menu?.gap} grid-cols-4 w-full`}>
@@ -81,11 +87,26 @@ export const SolgatesMenu = ({ menus, logoUrl }: SolgatesMenuProps) => {
                     <RiUser6Line className="mr-[10px]" color={colors.gray["600"]} size={18} />
                     <RiHeart3Line className="mr-[10px]" color={colors.gray["600"]} size={18} />
                     <RiShoppingBagLine className="mr-[10px]" color={colors.gray["600"]} size={18} />
+                    <RiSearch2Line className="mr-[10px] lg:hidden" color={colors.gray["600"]} size={18} />
                     <Input.Text
-                        className="h-[36px] rounded-[68px] w-[135px] bg-gray-50"
+                        className="md:hidden lg:block h-[36px] rounded-[68px] w-[135px] bg-gray-50"
                         placeholder="Search"
                         prefixIcon={<RiSearch2Line color={colors.gray["400"]} />}
                     />
+                </div>
+            </div>
+            <div className="flex md:hidden lg:hidden items-center h-[40px]">
+                <div className="flex flex-1">
+                    <RiMenu4Fill />
+                </div>
+                <div className="flex flex-1 justify-center">
+                    <img src={logoUrl} className="w-[60px]" />
+                </div>
+                <div className="flex flex-1 justify-end">
+                    <RiUser6Line className="mr-[10px]" color={colors.gray["600"]} size={14} />
+                    <RiHeart3Line className="mr-[10px]" color={colors.gray["600"]} size={14} />
+                    <RiShoppingBagLine className="mr-[10px]" color={colors.gray["600"]} size={14} />
+                    <RiSearch2Line color={colors.gray["600"]} size={14} />
                 </div>
             </div>
         </div>
