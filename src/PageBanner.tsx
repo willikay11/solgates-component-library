2963ca9c5f37aa72553a2714fcc3b1c15febee7f
William Kamau <william.kamuyu@gapstack.com>
2023-01-07 12:48:46 +0300

    Removed height prop

--- src/PageBanner.tsx
+++ src/PageBanner.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 export interface PageBannerProps {
     imageUrl: string;
-    height?: string;
     title?: string;
 }
 
-export const PageBanner = ({ imageUrl, title, height = 'h-full' }: PageBannerProps) => {
+export const PageBanner = ({ imageUrl, title }: PageBannerProps) => {
   return (
       <div className="w-full h-full relative rounded">
               <div style={{ backgroundImage: `url('${imageUrl}')`}}
-                   className={`relative flex rounded justify-center items-center bg-cover bg-center ${height}`}>
+                   className={`relative flex rounded justify-center items-center bg-cover bg-center h-full`}>
                   { title && <p className="z-20 text-6xl leading-none font-extrabold text-white">{title}</p>}
               </div>
           <div className="z-10 rounded absolute top-0 bottom-0 left-0 right-0" style={{ background: 'linear-gradient(0deg, rgba(31, 41, 55, 0.4), rgba(31, 41, 55, 0.4))' }} />
