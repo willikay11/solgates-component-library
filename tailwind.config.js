8bae6a484b86a093229c5a6b3d05ba4556c9dcc7
William Kamau <william.kamuyu@gapstack.com>
2023-01-07 22:52:26 +0300

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
   content: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors
+    },
     extend: {
       margin: {
         '50px': '50px'
