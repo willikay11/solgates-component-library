92f2c05f6dc32da14b3ca4dbca3a0bfa63b5bc9d
William Kamau <william.kamuyu@gapstack.com>
2023-01-15 10:06:17 +0300

    Updated solgates menu

--- src/BreadCrumbs.tsx
+++ src/BreadCrumbs.tsx
@@ -10,7 +10,7 @@ export interface BreadCrumbsProps {
 export const BreadCrumbs = ({items }: BreadCrumbsProps) => {
   return (
       <div className="inline-flex">
-          {items.map((item, index) => <span className={`text-xs leading-4 font-medium ${item.active ? 'text-orange-600 ml-1' : 'text-gray-800'}`}>{item.title} { index < items.length -1 ? ' /' : null }</span>)}
+          {items.map((item, index) => <span className={`text-xs leading-4 font-medium ${item.active ? 'text-orange-600 ml-1' : 'text-gray-800'}`}>{item.title} { index < items.length -1 ? ' / ' : null }</span>)}
       </div>
   );
 }
