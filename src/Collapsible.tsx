c770912021759bfd467912915a2d1ace629c4f27
William Kamau <william.kamuyu@gapstack.com>
2023-01-19 16:55:17 +0300

    Updated collapsible bordered

--- src/Collapsible.tsx
+++ src/Collapsible.tsx
@@ -11,11 +11,12 @@ export interface CollapsibleProps {
 }
 
 export const Collapsible = ({ closeIcon, openIcon, panelTitle, panelContent, open = false, bordered = true }: CollapsibleProps) => {
-  return(
+    console.log(bordered);
+   return(
       <div className="w-full max-w-md rounded-2xl bg-white">
           <Disclosure defaultOpen={open}>
             {({ open }) => (
-                <div className={`rounded bg-white border ${bordered ? 'border-gray-200' : ''}`}>
+                <div className={`rounded bg-white ${bordered ? 'border border-gray-200' : ''}`}>
                   <Disclosure.Button className={`flex w-full justify-between p-[15px] focus:outline-none ${open ? 'border-b-[1px]' : ''}`}>
                     <span className="text-xs leading-4 font-semibold text-gray-800">{panelTitle}</span>
                     {
