7206bfdd44246a6e78b9da113f9cf5ca0aaf5d0c
William Kamau <william.kamuyu@gapstack.com>
2023-02-14 05:47:08 +0300

    Updated collapsible props

--- src/Collapsible.tsx
+++ src/Collapsible.tsx
@@ -2,21 +2,22 @@ import React, {ReactNode} from "react";
 import { Disclosure } from '@headlessui/react'
 
 export interface CollapsibleProps {
-  open?: boolean;
-  closeIcon: ReactNode,
-  openIcon: ReactNode,
   panelTitle: string;
-  panelContent: ReactNode,
-  bordered?: boolean,
+  panelContent: ReactNode;
+  open?: boolean;
+  bordered?: boolean;
+  showTitleDivider?: boolean;
+  closeIcon?: ReactNode;
+  openIcon?: ReactNode;
 }
 
-export const Collapsible = ({ closeIcon, openIcon, panelTitle, panelContent, open = false, bordered = true }: CollapsibleProps) => {
+export const Collapsible = ({ closeIcon, openIcon, panelTitle, panelContent, showTitleDivider = true, open = false, bordered = true }: CollapsibleProps) => {
    return(
       <div className="w-full max-w-md rounded-2xl bg-white">
           <Disclosure defaultOpen={open}>
             {({ open }) => (
                 <div className={`rounded bg-white ${bordered ? 'border border-gray-200' : ''}`}>
-                  <Disclosure.Button className={`flex w-full justify-between p-[15px] focus:outline-none ${open ? 'border-b-[1px]' : ''}`}>
+                  <Disclosure.Button className={`flex w-full justify-between p-[15px] focus:outline-none ${open && showTitleDivider ? 'border-b-[1px]' : ''}`}>
                     <span className="text-xs leading-4 font-semibold text-gray-800">{panelTitle}</span>
                     {
                       open ? closeIcon : openIcon
