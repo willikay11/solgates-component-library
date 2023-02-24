26a373dced573d05bd6d36cb9130c944f4b002d8
William Kamau <william.kamuyu@gapstack.com>
2023-02-24 19:07:31 +0300

    Added tailwindcss form state

--- stories/Input.Text.stories.tsx
+++ stories/Input.Text.stories.tsx
@@ -14,6 +14,8 @@ const Template: Story<TextInputProps> = (args) => <Input.Text {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
+    name: "name",
+    error: "Error",
     prefixIcon: <IconContainer><User6Line color={colors.red["500"]} /></IconContainer>,
     suffixIcon: <IconContainer><User6Line color={colors.red["500"]} /></IconContainer>,
     clearIcon: <User6Line color={colors.red["500"]} />
