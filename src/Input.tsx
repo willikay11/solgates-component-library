a67dd85bedf7e7d0b925e98d4ec1c47aeb94df98
William Kamau <william.kamuyu@gapstack.com>
2023-02-25 06:05:58 +0300

    Added name to textarea

--- src/Input.tsx
+++ src/Input.tsx
@@ -29,12 +29,14 @@ const Password = ({ prefixIcon, iconRender, placeholder }: PasswordInputProps) =
 
 export interface TextAreaInputProps {
     rows: number;
+    name: string;
     placeholder?: string;
 }
 
-const TextArea = ({ rows, placeholder }: TextAreaInputProps) => {
+const TextArea = ({ rows, name, placeholder }: TextAreaInputProps) => {
     return (
         <textarea
+            name={name}
             placeholder={placeholder}
             className="w-full p-2.5 rounded bg-white border border-gray-200 outline-0 focus:border-orange-500 hover:border-orange-500 placeholder:text-xs placeholder:font-normal placeholder:leading-4 placeholder:text-gray-500 text-xs text-gray-800"
             rows={rows}
