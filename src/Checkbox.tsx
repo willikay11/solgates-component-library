0ceaa776ba9ba382971942450a5a60e55a859485
William Kamau <william.kamuyu@gapstack.com>
2023-05-02 06:59:09 +0300

    Added default checked to checkbox

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -6,15 +6,23 @@ export interface CheckboxProps {
   value: string,
   label?: ReactNode,
   disabled?: boolean,
-  checked?: boolean | undefined,
   defaultChecked?: boolean | undefined,
   onChange?: (event: any) => void;
 }
 
-export const Checkbox = ({id, name, label, value, disabled = false, onChange }: CheckboxProps) => {
+export const Checkbox = ({id, name, label, value, disabled = false, onChange, defaultChecked }: CheckboxProps) => {
   return(
       <div className="flex items-start items-center">
-        <input disabled={disabled} type="checkbox" id={id} name={name} value={value} onChange={(event) => onChange?.(event)} className={`${disabled ? 'cursor-not-allowed' : 'cursor-pointer'} h-[14px] w-[14px] accent-orange-600 mr-1 border-gray-800`} />
+        <input
+            disabled={disabled}
+            type="checkbox"
+            id={id}
+            name={name}
+            value={value}
+            onChange={(event) => onChange?.(event)}
+            defaultChecked={defaultChecked}
+            className={`${disabled ? 'cursor-not-allowed' : 'cursor-pointer'} h-[14px] w-[14px] accent-orange-600 mr-1 border-gray-800`}
+        />
           {label}
       </div>
   );
