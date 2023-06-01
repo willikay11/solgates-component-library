049c9bfde04d6cabb98f99367b5ca93e6b24e18f
William Kamau <william.kamuyu@gapstack.com>
2023-06-01 10:14:21 +0300

    Added ability to search in list box

--- src/Input/Select.tsx
+++ src/Input/Select.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, ReactNode, useEffect, useState } from 'react';
+import React, { Fragment, ReactNode, useEffect, useRef, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { Tag } from '../Tag';
 
@@ -20,6 +20,7 @@ export interface SelectInputProps {
   error?: string;
   border?: 'bordered' | 'borderless';
   defaultValue?: string;
+  showSearch?: boolean;
 }
 
 export const Select = ({
@@ -33,7 +34,10 @@ export const Select = ({
   error,
   border = 'bordered',
   defaultValue,
+  showSearch = false,
 }: SelectInputProps) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [searchValue, setSearchValue] = useState<string | null>(null);
   const [selectedItem, setSelectedItem] = useState<selectItem>(items[0]);
   const [multipleSelectedItems, setMultipleSelectedItem] = useState<
     selectItem[]
@@ -79,30 +83,59 @@ export const Select = ({
         <div className="relative w-full">
           <Listbox.Button
             id="headlessui-listbox-options-:r8:"
+            onClick={() => {
+              if (showSearch) {
+                console.log(searchInputRef);
+                searchInputRef.current?.focus();
+              }
+            }}
             className={`relative h-[3.125rem] w-full cursor-pointer rounded bg-white py-2 pl-3 pr-10 text-left ${
               border === 'bordered'
                 ? 'border border-gray-200 hover:border-orange-500 focus:border-orange-500'
                 : 'border border-transparent bg-transparent'
             } focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300  invalid:border-red-500 invalid:text-red-600 focus:invalid:border-red-500 focus:invalid:ring-red-500 sm:text-sm placeholder-opacity-100 focus-visible:placeholder-opacity-25`}
           >
-            <div className="inline-flex items-center h-full">
-              {prefixIcon}
-              {multiple ? (
-                <>
-                  <div className="mr-1" />
-                  {multipleSelectedItems.map((selectedItem) => (
-                    <Tag text={selectedItem.label} />
-                  ))}
-                </>
-              ) : (
-                <>
-                  <span className="ml-2 truncate">{selectedItem?.label}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    {arrowIcon}
-                  </span>
-                </>
-              )}
-            </div>
+            {({ open }) => (
+              <div className="inline-flex items-center w-full h-full">
+                {prefixIcon}
+                {multiple ? (
+                  <>
+                    <div className="mr-1" />
+                    {multipleSelectedItems.map((selectedItem) => (
+                      <Tag text={selectedItem.label} />
+                    ))}
+                    {showSearch && (
+                      <input
+                        autoFocus
+                        placeholder="Search..."
+                        className="ml-2 font-normal w-full focus:ring-0 focus:outline-0"
+                        onClick={(event) => event.stopPropagation()}
+                      />
+                    )}
+                  </>
+                ) : (
+                  <>
+                    {showSearch && open ? (
+                      <input
+                        autoFocus
+                        placeholder="Search..."
+                        value={searchValue ?? ''}
+                        className="ml-2 font-normal w-full focus:ring-0 focus:outline-0"
+                        onClick={(event) => event.stopPropagation()}
+                        onChange={(event) => setSearchValue(event.target.value)}
+                      />
+                    ) : (
+                      <span className="ml-2 truncate">
+                        {selectedItem?.label}
+                      </span>
+                    )}
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      {arrowIcon}
+                    </span>
+                  </>
+                )}
+              </div>
+            )}
           </Listbox.Button>
           <Transition
             as={Fragment}
@@ -111,32 +144,46 @@ export const Select = ({
             leaveTo="opacity-0"
           >
             <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-[1000]">
-              {items.map((item) => (
-                <Listbox.Option
-                  key={item.value}
-                  className={({ active }) =>
-                    `relative cursor-pointer select-none py-2 pl-4 pr-4 ${
-                      active ? 'bg-orange-100 text-orange-600' : 'text-gray-900'
-                    }`
+              {items
+                .filter((item) => {
+                  if (searchValue !== null) {
+                    return item?.label?.toLowerCase().includes(searchValue.toLowerCase());
                   }
-                  value={item.value}
-                  disabled={item.disabled || false}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected
-                            ? 'font-medium text-orange-600'
-                            : 'font-normal'
-                        }`}
-                      >
-                        {item.label}
-                      </span>
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
+                  return item;
+                })
+                .map((item) => (
+                  <Listbox.Option
+                    key={item.value}
+                    className={({ active }) =>
+                      `relative cursor-pointer select-none py-2 pl-4 pr-4 ${
+                        active
+                          ? 'bg-orange-100 text-orange-600'
+                          : 'text-gray-900'
+                      }`
+                    }
+                    value={item.value}
+                    disabled={item.disabled || false}
+                    onClick={() => {
+                      if (showSearch) {
+                        setSearchValue(item.label);
+                      }
+                    }}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected
+                              ? 'font-medium text-orange-600'
+                              : 'font-normal'
+                          }`}
+                        >
+                          {item.label}
+                        </span>
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
             </Listbox.Options>
           </Transition>
         </div>
