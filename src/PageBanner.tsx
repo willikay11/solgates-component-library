88c2f093835c426320d1314326e34562c57367af
William Kamau <william.kamuyu@gapstack.com>
2023-06-09 20:43:40 +0300

    Updated page banner

--- src/PageBanner.tsx
+++ src/PageBanner.tsx
@@ -35,7 +35,7 @@ export const PageBanner = ({
   return (
     <div className="w-full h-full rounded">
       <CloudimageProvider config={cloudImageConfig}>
-        <div className="h-auto relative">
+        <div className={`${type === 'image' ? 'h-auto' : 'h-full'} relative`}>
           {isValidElement(content) ? (
             content
           ) : type === 'image' ? (
