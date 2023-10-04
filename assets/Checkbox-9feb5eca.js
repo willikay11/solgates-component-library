39613c877deaaf920f23f4c14950d0077a630beb
William Kamau <william.kamuyu@gapstack.com>
2023-10-04 09:03:16 +0300

    Updates

---
+++ assets/Checkbox-9feb5eca.js
@@ -0,0 +1,6 @@
+import{j as o,a,F as c}from"./index-8a9ccefb.js";import{M as r,C as i}from"./index-a0a03e1d.js";import{C as m,D as h}from"./Checkbox.stories-7287dc18.js";import{u as s}from"./index-3440ec21.js";import"./client-128ae93f.js";import"./iframe-4c1fc1e8.js";import"../sb-preview/runtime.js";import"./index-d37d4223.js";import"./index-c3b73a36.js";import"./index-356e4a49.js";function n(t){const e=Object.assign({h1:"h1",p:"p"},s(),t.components);return a(c,{children:[o(r,{of:m}),`
+`,o(e.h1,{id:"checkbox",children:"Checkbox"}),`
+`,o(e.p,{children:"A checkbox is a square box that can be activated or deactivated when ticked."}),`
+`,o(e.p,{children:"Use checkboxes to select one or more options from a list of choices."}),`
+`,o(i,{of:h})]})}function M(t={}){const{wrapper:e}=Object.assign({},s(),t.components);return e?o(e,Object.assign({},t,{children:o(n,t)})):n(t)}export{M as default};
+//# sourceMappingURL=Checkbox-9feb5eca.js.map
