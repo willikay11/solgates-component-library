262b99c54a2d2055b35f3564d73c8f086ac88624
William Kamau <william.kamuyu@gapstack.com>
2023-10-04 09:38:49 +0300

    added deployment file

---
+++ storybook-static/assets/Checkbox.stories-7287dc18.js
@@ -0,0 +1,13 @@
+import{C as c,j as l}from"./index-8a9ccefb.js";const r={title:"Data Entry/Checkbox",component:c},e={args:{name:"checkbox",id:"checkbox",label:l("label",{className:"text-xs leading-4 text-gray-800 font-normal",children:"Accept terms & conditions"}),disabled:!1,defaultValue:"checkbox",defaultChecked:!0}};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    name: 'checkbox',
+    id: 'checkbox',
+    label: <label className="text-xs leading-4 text-gray-800 font-normal">
+        Accept terms & conditions
+      </label>,
+    disabled: false,
+    defaultValue: 'checkbox',
+    defaultChecked: true
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const s=["Default"],n=Object.freeze(Object.defineProperty({__proto__:null,Default:e,__namedExportsOrder:s,default:r},Symbol.toStringTag,{value:"Module"}));export{n as C,e as D};
+//# sourceMappingURL=Checkbox.stories-7287dc18.js.map
