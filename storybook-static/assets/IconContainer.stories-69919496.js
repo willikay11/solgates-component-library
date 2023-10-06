87998e24eae9dbbe582edae148fa20d738006906
William Kamau <william.kamuyu@gapstack.com>
2023-10-06 19:40:17 +0300

    Added invisible class to image tile

--- storybook-static/assets/IconContainer.stories-69919496.js
+++
@@ -1,4 +0,0 @@
-import{d as s,j as t,U as c}from"./index-bf72dc8c.js";import{c as i}from"./colors-eb3cbc37.js";import"./client-128ae93f.js";const u={title:"Data Display/Icon Container",component:s},l=n=>t(s,{...n}),r=l.bind({children:t(c,{color:i.gray[600]})});var e,o,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`Template.bind({
-  children: <User6Line color={colors.gray[600]} />
-})`,...(a=(o=r.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
-//# sourceMappingURL=IconContainer.stories-69919496.js.map
