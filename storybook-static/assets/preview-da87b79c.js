c0a5eb22a72b27f44e2f6e4a331c16cbb15fc4ee
William Kamau <william.kamuyu@gapstack.com>
2023-10-10 20:30:37 +0300

    Updated github release

--- storybook-static/assets/preview-da87b79c.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-23d5de9c.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-a4188942.js"),["./DocsRenderer-3PUGWF3O-a4188942.js","./iframe-23d5de9c.js","./client-128ae93f.js","./react-18-d9285b47.js","./index-e3069409.js","./index-d37d4223.js","./index-c3b73a36.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-da87b79c.js.map
