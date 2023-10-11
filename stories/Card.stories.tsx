23ae15c0986465296a0fe00e57bd38c31e445e06
William Kamau <william.kamuyu@gapstack.com>
2023-10-11 21:34:01 +0300

    Added card docs

--- stories/Card.stories.tsx
+++ stories/Card.stories.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
-import {
-  Button,
-  ButtonTypes,
-  Card,
-  CardProps,
-  IconContainer,
-  Input,
-  User6Line,
-} from '../src';
+import {Meta, StoryObj} from '@storybook/react';
+import {Button, ButtonTypes, Card, CARD_TYPE, IconContainer, Input, User6Line,} from '../src';
 import colors from 'tailwindcss/colors';
 
 const meta: Meta = {
@@ -17,84 +9,225 @@ const meta: Meta = {
 };
 
 export default meta;
+type Story = StoryObj<typeof Card>;
 
-const Template: Story<CardProps> = (args) => (
-  <div className="sm:w-full md:w-full lg:w-96 xl:w-72 2xl:w-64">
-    <Card {...args} />
-  </div>
-);
+export const Default: Story = {
+    render: (args) => (
+        <Card {...args}>
+            <div className="w-full">
+                <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-2">
+                    <Input.Text
+                        placeholder="First Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                    <Input.Text
+                        placeholder="Last Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
 
-export const Default = Template.bind({});
-Default.args = {
-  title: 'Card title',
-  extraContent: (
-    <Button onClick={() => console.log('clicked')} type={ButtonTypes.link}>
-      View
-    </Button>
-  ),
-  children: (
-    <div className="w-full">
-      <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-2">
-        <Input.Text
-          placeholder="First Name"
-          prefixIcon={
-            <IconContainer>
-              <User6Line color={colors.green['500']} />
-            </IconContainer>
-          }
-        />
-        <Input.Text
-          placeholder="Last Name"
-          prefixIcon={
-            <IconContainer>
-              <User6Line color={colors.green['500']} />
-            </IconContainer>
-          }
-        />
-      </div>
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Region"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
 
-      <div className="grid grid-cols-1 mb-2">
-        <Input.Text
-          placeholder="Region"
-          prefixIcon={
-            <IconContainer>
-              <User6Line color={colors.blue['500']} />
-            </IconContainer>
-          }
-        />
-      </div>
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Apartment/Office"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.cyan['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
 
-      <div className="grid grid-cols-1 mb-2">
-        <Input.Text
-          placeholder="Apartment/Office"
-          prefixIcon={
-            <IconContainer>
-              <User6Line color={colors.cyan['500']} />
-            </IconContainer>
-          }
-        />
-      </div>
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Phone"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
 
-      <div className="grid grid-cols-1 mb-2">
-        <Input.Text
-          placeholder="Phone"
-          prefixIcon={
-            <IconContainer>
-              <User6Line color={colors.blue['500']} />
-            </IconContainer>
-          }
-        />
-      </div>
+                <div className="grid grid-cols-1 mb-2">
+                    <Button
+                        onClick={() => console.log('clicked')}
+                        type={ButtonTypes.primary}
+                        block
+                    >
+                        Save & Continue
+                    </Button>
+                </div>
+            </div>
+        </Card>
+    ),
+    args: {
+        title: "Card Title"
+    }
+}
 
-      <div className="grid grid-cols-1 mb-2">
-        <Button
-          onClick={() => console.log('clicked')}
-          type={ButtonTypes.primary}
-          block
-        >
-          Save & Continue
-        </Button>
-      </div>
-    </div>
-  ),
-};
+export const ExtraContent: Story = {
+    render: (args) => (
+        <Card {...args}>
+            <div className="w-full">
+                <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-2">
+                    <Input.Text
+                        placeholder="First Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                    <Input.Text
+                        placeholder="Last Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Region"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Apartment/Office"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.cyan['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Phone"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Button
+                        onClick={() => console.log('clicked')}
+                        type={ButtonTypes.primary}
+                        block
+                    >
+                        Save & Continue
+                    </Button>
+                </div>
+            </div>
+        </Card>
+    ),
+    args: {
+        title: "Card Title",
+        extraContent: <Button type={ButtonTypes.link}>Cancel</Button>
+    }
+}
+
+export const typeShadow: Story = {
+    render: (args) => (
+        <Card {...args}>
+            <div className="w-full">
+                <div className="grid gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-2">
+                    <Input.Text
+                        placeholder="First Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                    <Input.Text
+                        placeholder="Last Name"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.green['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Region"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Apartment/Office"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.cyan['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Input.Text
+                        placeholder="Phone"
+                        prefixIcon={
+                            <IconContainer>
+                                <User6Line size={14} color={colors.blue['500']} />
+                            </IconContainer>
+                        }
+                    />
+                </div>
+
+                <div className="grid grid-cols-1 mb-2">
+                    <Button
+                        onClick={() => console.log('clicked')}
+                        type={ButtonTypes.primary}
+                        block
+                    >
+                        Save & Continue
+                    </Button>
+                </div>
+            </div>
+        </Card>
+    ),
+    args: {
+        title: "Card Title",
+        type: CARD_TYPE.shadow
+    }
+}
