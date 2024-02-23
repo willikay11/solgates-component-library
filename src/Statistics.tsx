111ff8791d0bf2b612a8ad166825b0e7255d0c17
will Kamau <willikay11@gmail.com>
2024-02-24 00:50:11 +0300

    Fixed statistics col spans

--- src/Statistics.tsx
+++ src/Statistics.tsx
@@ -14,6 +14,35 @@ export interface StatisticsProps {
   stats: stat[];
 }
 
+function COL_SPANS(span: number) {
+  switch(span) {
+    case 1:
+      return 'col-span-1';
+    case 2:
+      return 'col-span-2';
+    case 3:
+      return 'col-span-3';
+    case 4:
+      return 'col-span-4';
+    case 5:
+      return 'col-span-5';
+    case 6:
+      return 'col-span-6';
+    case 7:
+      return 'col-span-7';
+    case 8:
+      return 'col-span-8';
+    case 9:
+      return 'col-span-9';
+    case 10:
+      return 'col-span-10';
+    case 11:
+      return 'col-span-11';
+    default:
+      return 'col-span-12';
+  }
+}
+
 export const Statistics = ({ title, stats }: StatisticsProps) => {
   return (
     <div className="rounded w-full bg-white border-[1px] border-gray-200 p-3">
@@ -37,7 +66,7 @@ export const Statistics = ({ title, stats }: StatisticsProps) => {
 
       <div className="grid grid-cols-12 gap-1">
         {stats.map((stat, index) => (
-          <div key={index} className={`grid col-span-${stat.span}`}>
+          <div key={index} className={`grid ${COL_SPANS(stat.span)}`}>
             <div className="flex flex-row justify-start items-center">
               <div className="flex flex-col">
                 <div className="mb-[5px]">{stat.icon}</div>
