7f4e1eb76f44835807a6700861ff36d4167a5893
William Kamau <william.kamuyu@gapstack.com>
2024-06-07 12:02:57 +0300

    Created docs for product tile

--- src/ProductTile/imageLoader.tsx
+++ src/ProductTile/imageLoader.tsx
@@ -11,13 +11,6 @@ const ImageLoader = ({ height = 'h-auto' }: ImageLoaderProps) => {
       <div className={`rounded ${height} w-full`}>
         <Skeleton className="h-full"/>
       </div>
-    // <img
-    //   alt="Image"
-    //   className={`rounded ${height}`}
-    //   style={{ width: '100%', backgroundColor: '#f4f4f4' }}
-    //   loading="eager"
-    //   src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-    // />
   );
 };
 
--- stories/ProductTile.stories.tsx
+++ stories/ProductTile.stories.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import {
-  // IconContainer,
   PRODUCT_TYPE,
   ProductTileProps,
   ProductTile,
-  // User6Line,
 } from '../src';
-// import colors from 'tailwindcss/colors';
 
 const meta: Meta = {
   title: 'Data Display/Product Tile',
@@ -16,7 +13,7 @@ const meta: Meta = {
 
 export default meta;
 const Template: Story<ProductTileProps> = (args) => (
-  <div className="grid gap-2 grid-cols-2 md:gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-4 h-screen">
+  <div className="grid gap-2 grid-cols-2 md:gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-4">
     <ProductTile {...args} />
     <ProductTile {...args} />
     <ProductTile {...args} />
@@ -50,13 +47,6 @@ Product.args = {
   type: PRODUCT_TYPE.PRODUCT,
 };
 
-// export const Collection = Template.bind({});
-// Collection.args = {
-//   type: PRODUCT_TYPE.COLLECTION,
-//   imageUrl: 'high-tops-are-back.png',
-//   name: 'Jordans',
-// };
-
 export const Carousel = Template.bind({});
 Carousel.args = {
   type: PRODUCT_TYPE.CAROUSEL,
@@ -73,20 +63,3 @@ Carousel.args = {
     'https://res.cloudinary.com/dp1buffig/image/upload/v1695717367/solgates/Products/Shorts/Nike%20Club%20Fleece/jqlzmtwfer7ck0tqkbbl.webp',
   ],
 };
-
-// export const Content = Template.bind({});
-// Content.args = {
-//   type: PRODUCT_TYPE.CONTENT,
-//   content: (
-//     <div className="h-full w-full bg-orange-100 rounded-[5px] border-dashed border-orange-600 border-[1px] flex flex-1 items-center cursor-pointer">
-//       <div className="flex flex-col items-center justify-center w-full">
-//         <IconContainer borderRadius="rounded-full" bgColor="bg-orange-200">
-//           <User6Line color={colors.orange['600']} />
-//         </IconContainer>
-//         <p className="text-xs leading-4 font-normal text-orange-600 text-center mt-2.5">
-//           Got a plug you would like us to add? Please share their details
-//         </p>
-//       </div>
-//     </div>
-//   ),
-// };
