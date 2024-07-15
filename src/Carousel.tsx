3ce7d42f333e49a12e3e4e859753fa3277b4886d
William Kamau <william.kamuyu@gapstack.com>
2024-07-15 10:03:22 +0300

    removed slick carousel library

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,7 +1,5 @@
 import { isValidElement, ReactNode, useRef, useState } from 'react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { ArrowLeftSLine, ArrowRightSLine } from './Icons';
 import colors from './Colors';
 
